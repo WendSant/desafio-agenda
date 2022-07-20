692cd8e23534bca18a2e01730902d43bc70da89d
Wendson <wendsonsantanajps@gmail.com>
2022-07-20 15:11:08 -0300

    Adicionado cadastro e login com tratamento

--- tarefa/views.py
+++ tarefa/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from usuarios.models import Usuario
 
 
-def cadastrar(request):
-    return HttpResponse('olá')
+def index(request):
+    if request.session.get('usuario'):
+        usuario = Usuario.objects.get(id = request.session['usuario']).nome
+        return HttpResponse(f'olá {usuario}')
+    else:
+        return redirect('/auth/login/?status=3')
---
+++ usuarios/urls.py
@@ -0,0 +1,15 @@
+
+
+from . import views
+from django.urls import path
+
+
+urlpatterns = [
+    
+    path('login/', views.login , name='login'),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('valida_cadastro/', views.valida_cadastro, name='valida_cadastro'),
+    path('valida_login/', views.valida_login, name='valida_login'),
+    path('sair/', views.sair, name='sair')
+
+]
